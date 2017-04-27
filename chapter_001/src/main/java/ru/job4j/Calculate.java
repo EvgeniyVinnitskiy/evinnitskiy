ae25cef21231d6743fb6a02a445de27e7ee07c81
EvgeniyVinnitskiy <report-2k8@yandex.ru>
2017-04-27 20:20:33 +0300

    task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,18 @@
 package ru.job4j;
 
+/**Сalculate task
+*
+* @author Evgeniy Vinnitskiy (report-2k8@yandex.ru)
+* @version $1.0$
+* @since 1.0
+*/
+
 public class Calculate {
+	/**
+     * Metod Main.
+     * @param args - args.
+     */
+	
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,9 @@
+
+/**
+* Package for calculate task.
+*
+* @author Evgeniy Vinnitskiy (mailto:report-2k8@yandex.ru)
+* @version $1.0$
+* @since 1.0
+*/
+package ru.job4j;
