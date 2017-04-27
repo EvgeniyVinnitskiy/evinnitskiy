ef5c987c9a9790a3987d0e25966090ed04ffdc66
EvgeniyVinnitskiy <report-2k8@yandex.ru>
2017-04-27 21:09:31 +0300

    task 4122

---
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -0,0 +1,36 @@
+package ru.job4j;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+* Test.
+*
+* @author Evgeniy Vinnitskiy (mailto:report-2k8@yandex.ru)
+* @version $1.0$
+* @since 1.0
+*/
+public class CalculateTest {
+/**
+* Test add.
+*/
+@Test
+public void whenAddOneToOneThenTwo() {
+ByteArrayOutputStream out = new ByteArrayOutputStream();
+System.setOut(new PrintStream(out));
+Calculate.main(null);
+assertThat(
+out.toString(),
+is(
+String.format(
+"Hello World%s",
+System.getProperty("line.separator")
+)
+)
+);
+}
+}
+ 
