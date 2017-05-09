7c318a00fe8fc44c2e4253495d8c2fdcd683ccef
EvgeniyVinnitskiy <report-2k8@yandex.ru>
2017-05-09 20:57:42 +0400

    task 9461

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -3,8 +3,8 @@ package ru.job4j.condition;
  /**
 * Класс Triangle.
 * @author Evgeniy Vinnitskiy (mailto:report-2k8@yandex.ru)
-* @version $1.0$
-* @since 30.04.2017
+* @version $1.1$
+* @since 09.05.2017
 */
 public class Triangle {
     /**
@@ -41,27 +41,6 @@ public class Triangle {
 			+ Math.pow(firstPoint.getY() - secondPoint.getY(), 2));
     }
     /**
-     *Calculates the perimeter of the triangle.
-     *@return the length of the perimeter of the triangle.
-     */
-    public double perimeter() {
-	return Triangle.lengthSide(this.a, this.b)
-	    + Triangle.lengthSide(this.b, this.c)
-	    + Triangle.lengthSide(this.c, this.a);
-    }
-    /**
-     *Check for existence of the triangle.
-     *@return true, if triangle exist.
-     */
-    public boolean checkTriangleExist() {
-	double lengtABSide = Triangle.lengthSide(this.a, this.b);
-	double lengtBCSide = Triangle.lengthSide(this.b, this.c);
-	double lengtCASide = Triangle.lengthSide(this.c, this.a);
-	return lengtABSide < lengtBCSide +  lengtCASide
-		&& lengtBCSide < lengtABSide + lengtCASide
-		&& lengtCASide < lengtABSide + lengtBCSide;
-    }
-    /**
      *Method calculates the area of a trangle.
      *@return - the area triangle, if triangle does not exist then return -1.
      */
@@ -70,8 +49,10 @@ public class Triangle {
 	double lengtABSide = Triangle.lengthSide(this.a, this.b);
 	double lengtBCSide = Triangle.lengthSide(this.b, this.c);
 	double lengtCASide = Triangle.lengthSide(this.c, this.a);
-	double halfPerimeter = perimeter() / 2;
-	if (checkTriangleExist()) {
+	double halfPerimeter = (Triangle.lengthSide(this.a, this.b) + Triangle.lengthSide(this.b, this.c)
+	    + Triangle.lengthSide(this.c, this.a)) / 2;
+	if (lengtABSide < lengtBCSide +  lengtCASide && lengtBCSide < lengtABSide + lengtCASide
+		&& lengtCASide < lengtABSide + lengtBCSide) {
 	    areaTriangle = Math.sqrt(halfPerimeter * (halfPerimeter - lengtABSide)
 				    * (halfPerimeter - lengtBCSide)
 				    * (halfPerimeter - lengtCASide));
