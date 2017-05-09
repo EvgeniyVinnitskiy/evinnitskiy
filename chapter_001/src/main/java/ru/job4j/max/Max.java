f1bd85a8b0fd7e399ee31dfbe3161489fb2084b2
EvgeniyVinnitskiy <report-2k8@yandex.ru>
2017-05-09 15:30:56 +0400

    task 190

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -3,8 +3,8 @@ package ru.job4j.max;
 * Package for maximum.
 *
 * @author Evgeniy Vinnitskiy (mailto:report-2k8@yandex.ru)
-* @version $1.0$
-* @since 29.04.2017
+* @version $2.0$
+* @since 09.05.2017
 */
 public class Max {
 	/**
@@ -16,4 +16,14 @@ public class Max {
     public int max(int first, int second) {
     return first > second ? first : second;
     }
+	/**
+    * Metod public.
+    * @param first first number
+	* @param second second number
+	* @param third third number
+	* @return maximum
+     */
+	public int max(int first, int second, int third) {
+	return max(first, max(second, third));
+	}
 }
--- chapter_001/src/test/java/ru/job4j/max/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -7,19 +7,30 @@ import static org.junit.Assert.assertThat;
 * Package for maximum test.
 *
 * @author Evgeniy Vinnitskiy (mailto:report-2k8@yandex.ru)
-* @version $1.0$
-* @since 29.04.2017
+* @version $2.0$
+* @since 09.05.2017
 */
 public class Max {
 	/**
 	* Test Max.
 	*/
     @Test
-    public void whenAddOnePlusOneThenTwo() {
+    public void whenMaxSevenAndTreeThenSeven() {
         Max maxi = new Max();
         maxi.max(7, 3);
         int result = maxi.getResult();
         int expected = 7;
+        assertThat(result, is(expected));
+    }
+	/**
+	* Test Maximum of three numbers.
+	*/
+    @Test
+    public void whenMaxThreeAndFiveAndNineThenNine() {
+        Max maxi = new Max();
+        maxi.max(3, 5, 9);
+        int result = maxi.getResult();
+        int expected = 9;
         assertThat(result, is(expected));
     }
 }
